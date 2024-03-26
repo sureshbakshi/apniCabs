9e7001d1e9fb5fda29538cbec482f87e25957fea
suresh besta <sureshbakshi88@gmail.com>
2024-03-27 01:34:17 +0530

    fix: endpoint https enabled

--- src/navigation/GetNavigation.js
+++ src/navigation/GetNavigation.js
@@ -1,18 +1,24 @@
 import { useSelector } from 'react-redux';
-import { isDriver, isUser } from '../util';
+import { isDriver, isUser, showErrorMessage } from '../util';
 import { isEmpty } from 'lodash';
 import UserTabNavigator from './userTabNavigation';
 import DriverTabNavigator from './driverTabNavigation';
 import LoginNavigator from './loginNavigation';
 import useNotifications from '../hooks/useNotifications';
+import useLogout from '../hooks/useLogout';
 
 export const GetAuthRoutes = () => {
     const isDriverLogged = isDriver()
     const isUserLogged= isUser()
+    const {logOut} = useLogout()
    if (isDriverLogged) {
         route = <DriverTabNavigator />;
     } else if (isUserLogged) {
       route = <UserTabNavigator />;
+    } else{
+        logOut()
+        showErrorMessage(`You don't have enough permission to login.`)
+        route = <LoginNavigator />;
     }
     return route
 }
--- src/sockets/socketConfig.js
+++ src/sockets/socketConfig.js
@@ -2,7 +2,8 @@
 import io from 'socket.io-client';
 
 // baseUrl: 'http://192.168.0.101:3000/', //rajesh IP
-const socketUri = 'http://ec2-65-0-142-176.ap-south-1.compute.amazonaws.com:8080/'
+// const socketUri = 'http://ec2-65-0-142-176.ap-south-1.compute.amazonaws.com:8080/'
+const socketUri =  "https://apnicabi.com"
 // const socketUri = 'http://192.168.0.105:3000'
 const socket = io(socketUri, {
   // Additional configuration options can be set here
