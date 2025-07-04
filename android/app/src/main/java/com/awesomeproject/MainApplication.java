c2a53b7f8178e497a63eee1beb001fc418813e5d
suresh besta <sureshbakshi88@gmail.com>
2025-07-04 23:30:00 +0530

    chore: react native version bump to 0.72.4

--- android/app/src/main/java/com/awesomeproject/MainApplication.java
+++ android/app/src/main/java/com/awesomeproject/MainApplication.java
@@ -59,7 +59,9 @@ public class MainApplication extends Application implements ReactApplication {
     SoLoader.init(this, /* native exopackage */ false);
     if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
       // If you opted-in for the New Architecture, we load the native entry point for this app.
-      DefaultNewArchitectureEntryPoint.load();
+      // DefaultNewArchitectureEntryPoint.load();
+      DefaultNewArchitectureEntryPoint.getFabricEnabled();
+
     }
     ReactNativeFlipper.initializeFlipper(this, getReactNativeHost().getReactInstanceManager());
   }
