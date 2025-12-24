eb54119fe46d9234886f54cd44d39e17fe360f8e
“rraj5422” <rraj5422@gmail.com>
2025-12-24 15:00:32 +0530

    refactor: screen container Ui changes

--- src/components/ScreenContainer.js
+++ src/components/ScreenContainer.js
@@ -5,20 +5,20 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 const ScreenContainer = props => {
   const { children } = props;
   return (
-    <SafeAreaView style={{
-      flex: 1, backgroundColor: '#fff',
-    }}>
-      <KeyboardAvoidingView
-        behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    // <SafeAreaView style={{
+    //   flex: 1, backgroundColor: '#fff',
+    // }}>
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+    >
+      <ScrollView
+        contentInsetAdjustmentBehavior="automatic"
+        keyboardShouldPersistTaps="always"
       >
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          keyboardShouldPersistTaps="always"
-        >
-          {children}
-        </ScrollView>
-      </KeyboardAvoidingView>
-    </SafeAreaView>
+        {children}
+      </ScrollView>
+    </KeyboardAvoidingView>
+    // </SafeAreaView>
   );
 };
 
