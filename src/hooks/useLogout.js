812ff26d97a3e9836f9fb346af643f346700efb6
“rraj5422” <rraj5422@gmail.com>
2025-12-27 12:24:16 +0530

    fix: removed  react-native-keychain package and updated code accordingly

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -1,10 +1,8 @@
 import { useDispatch } from "react-redux";
-import { useAuthContext } from "../context/Auth.context";
 import { clearAuthData } from "../slices/authSlice";
-import {disconnectSocket} from '../sockets/socketConfig'
+import { disconnectSocket } from '../sockets/socketConfig'
 // import { isDriver } from '../util';
 export default useLogout = () => {
-  const { signOut } = useAuthContext();
   // const isDriverLogged = isDriver()
   const dispatch = useDispatch();
   // const { requestAlertHandler } = useRequestAlertHandler();
@@ -16,11 +14,8 @@ export default useLogout = () => {
 
   const logOut = () => {
     try {
-      const sucess = signOut();
-      if (sucess ) {
-        logOutHandler()
-        // isDriverLogged ? logOutHandler() : requestAlertHandler(logOutHandler)
-      }
+      logOutHandler()
+      // isDriverLogged ? logOutHandler() : requestAlertHandler(logOutHandler)
     } catch (error) {
       console.error(error);
     }
