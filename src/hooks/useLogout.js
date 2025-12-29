c5e8ae26eef45fdb11d2fe2eaa084c1a7cb81645
suresh besta <sureshbakshi88@gmail.com>
2025-12-29 15:34:25 +0530

    refactor: clean up unused code and comments in useLogout and FindCaptainPage components

--- src/hooks/useLogout.js
+++ src/hooks/useLogout.js
@@ -2,11 +2,8 @@ import { useDispatch } from "react-redux";
 import { clearAuthData } from "../slices/authSlice";
 import { disconnectSocket } from '../sockets/socketConfig'
 import { stopForegroundLocation } from "../util/foregroundLocationService";
-// import { isDriver } from '../util';
 export default useLogout = () => {
-  // const isDriverLogged = isDriver()
   const dispatch = useDispatch();
-  // const { requestAlertHandler } = useRequestAlertHandler();
 
   const logOutHandler = async () => {
     disconnectSocket();
@@ -17,7 +14,6 @@ export default useLogout = () => {
   const logOut = () => {
     try {
       logOutHandler()
-      // isDriverLogged ? logOutHandler() : requestAlertHandler(logOutHandler)
     } catch (error) {
       console.error(error);
     }
